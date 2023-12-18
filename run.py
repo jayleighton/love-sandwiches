5df363b65811a9d416f8c02f2ef85e5061e1f9b9
jayleighton <>
2023-12-18 15:08:45 +0000

    Collect sales data fort last 5 marketss in lists if columns

--- run.py
+++ run.py
@@ -82,6 +82,21 @@ def update_worksheet(data, worksheet):
     print(f"{worksheet} worksheet updated successfully.\n")
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sakes worksheet, collecting
+    the last 5 entries for each sandiwch and returns the data
+    as a list of lists
+    """
+    sales = SHEET.worksheet('sales')
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -93,4 +108,5 @@ def main():
     update_worksheet(new_surplus_data, 'surplus')
 
 
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
